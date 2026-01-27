481a515c5d2a9b1ecbab280b3a393bf3b6ca3129
estebanmariot <esteban.mariot@fastcube.fr>
2026-01-27 15:34:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener("fetch", (event) => {
-    console.log('Fetching : ${event.request.url}, Mode : ${event.request.mode}');
+    console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
 
     if (event.request.mode === 'navigate'){
         event.respondWith((async () => {
