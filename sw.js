199509f8fdd04df26f034692aa4620bb4d79840f
estebanmariot <esteban.mariot@fastcube.fr>
2026-01-27 15:36:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
+// Ajout recommandé
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installé');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activé');
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
     console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
-
     if (event.request.mode === 'navigate'){
         event.respondWith((async () => {
             try{
-                const preloadResponse = await event.preloadResponse
+                const preloadResponse = await event.preloadResponse;
                 if (preloadResponse) {
                     return preloadResponse;
                 }
-
-                return await fetch(event.request)
+                return await fetch(event.request);
             } catch(e) {
                 return new Response("TODOLIST");
             }
-        }))
+        })());
     }
 });
